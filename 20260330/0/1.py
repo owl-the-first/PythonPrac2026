f8af7e26fadf2fc2def6008e13c2595ec8083353
Денис Кульпин <74856314+owl-the-first@users.noreply.github.com>
2026-03-30 15:32:08 +0300

    Classwork

---
+++ 20260330/0/1.py
@@ -0,0 +1,17 @@
+"""XD.
+
+Hello.
+"""
+
+from math import *
+
+sin(0)
+
+line = "HELLO 1223             TEST lol/0          LMAO:              'ok' HEY my name is Kirill"
+
+def func():
+    """A fun()
+
+    Second line.
+    """
+    print("Hello")
